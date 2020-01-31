e873426643216dd01377bd3d4d6a7fb4d6b3b474
Nicolas Dimarco <nicolasdimarco@fiqus.com>
2020-01-31 16:26:04 -0300

    help text added to management command

--- empresa/management/commands/init_empresa.py
+++ empresa/management/commands/init_empresa.py
@@ -4,11 +4,11 @@ from empresa.models import Empresa
 
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Create empresa'
 
     def add_arguments(self, parser):
-        parser.add_argument('nro_doc', type=str)
-        parser.add_argument('nombre', type=str)
+        parser.add_argument('nro_doc', type=str, help="CUIT or DNI number")
+        parser.add_argument('nombre', type=str, help="Short name, not business name")
 
     def handle(self, *args, **options):
         empresa = Empresa(nro_doc=options['nro_doc'], nombre=options['nombre'])
