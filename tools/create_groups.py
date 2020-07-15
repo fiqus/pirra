b48c256e5b675ad3a1923b63c28ee87445f8ebbd
MJoaaquin <joaquinmansilla@fiqus.com>
2020-07-15 17:28:10 -0300

    arreglando script para crear grupos

--- tools/create_groups.py
+++ tools/create_groups.py
@@ -12,23 +12,33 @@ with open("./grupos.csv", "r") as grupos_csv:
         container_name = "pirra_" + str(i)
         
         # create db and migrate
-        subprocess.call(["docker-compose", "exec", "-u", "postgres", "postgres", "createdb", db_name])
-        subprocess.call(["docker-compose", "exec", container_name, "python3", "manage.py", "migrate"])
+        p1 = subprocess.Popen(["docker-compose", "exec", "-u", "postgres", "postgres", "createdb", db_name])
+        p1.wait()
+
+        p2 = subprocess.Popen(["docker-compose", "exec", container_name, "python3", "manage.py", "migrate"])
+        p2.wait()
 
         # create a user for the teacher in each group
-        subprocess.call(
+        p3 = subprocess.Popen(
             [
                 "docker-compose", "exec", container_name, "python3", "manage.py", "shell", "-c", 
                 "from django.contrib.auth import get_user_model; User = get_user_model(); \
                     User.objects.create_superuser('carolina', 'carolina.guglielmotti@cpem17.com.ar', 'carito2626')"
             ]
         )
-        
+        p3.wait()
+
         # create a user for the group
         create_group_user = "from django.contrib.auth import get_user_model;\
             User = get_user_model(); User.objects.create_superuser('" + group_name + "', '', '" + group_cuit + "')"
-        subprocess.call(["docker-compose", "exec", container_name, "python3", "manage.py", "shell", "-c", create_group_user])
+
+        print("CREATING USER " + group_name + " TO GROUP " + group_name + " CREATED!")
+
+        p4 = subprocess.Popen(["docker-compose", "exec", container_name, "python3", "manage.py", "shell", "-c", create_group_user])
+        p4.wait()
 
         # create company
-        subprocess.call(["docker-compose", "exec", container_name, "python3", "manage.py", "init_empresa", group_cuit, group_name])
+        p5 = subprocess.Popen(["docker-compose", "exec", container_name, "python3", "manage.py", "init_empresa", group_cuit, group_name])
+        p5.wait()
+
         print("GROUP " + group_name + " CREATED!")
