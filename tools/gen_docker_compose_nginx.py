2481049f58fc8ab2fd0e666af3b419e2f095eb95
MJoaaquin <joaquinmansilla@fiqus.com>
2021-10-28 16:10:51 -0300

    diferentes yml y nginx por institucion

--- tools/gen_docker_compose_nginx.py
+++ tools/gen_docker_compose_nginx.py
@@ -1,11 +1,14 @@
 import csv
 import shutil
 import os
+import sys
 
 from jinja2 import Environment, FileSystemLoader
 
+institution_name = sys.argv[1]
+
 # get groups and format for template
-with open("./all_groups.csv", "r") as grupos_csv:
+with open(f"./{institution_name}.csv", "r") as grupos_csv:
     csv_reader = csv.reader(grupos_csv)
     groups = []
 
@@ -24,18 +27,18 @@ env = Environment(loader=file_loader)
 
 def create_file(templete_name, file_name):
     templete = env.get_template(templete_name)
-    templete.stream(groups=groups).dump(file_name)
+    templete.stream(groups=groups, institution_name=institution_name).dump(file_name)
 
-create_file("docker_compose_template.yml", "./outputs/docker-compose.yml")
-create_file("nginx_template.conf", "./outputs/nginx.conf")
-create_file("env_templete.j2", "./outputs/.env")
+create_file("docker_compose_template.yml", f"./outputs/{institution_name}.yml")
+create_file("nginx_template.conf", f"./outputs/nginx_{institution_name}.conf")
+create_file("env_templete.j2", f"./outputs/.env_{institution_name}")
 
 # move to root (if it exists replaces it)
 src = "./outputs/"
 dst = "./../"
-docker_filename = "docker-compose.yml"
-nginx_filename = "nginx.conf"
-env_filename = ".env"
+docker_filename = f"{institution_name}.yml"
+nginx_filename = f"nginx_{institution_name}.conf"
+env_filename = f".env_{institution_name}"
 shutil.move(os.path.join(src, docker_filename), os.path.join(dst, docker_filename))
 shutil.move(os.path.join(src, nginx_filename), os.path.join(dst, nginx_filename))
 shutil.move(os.path.join(src, env_filename), os.path.join(dst, env_filename))
