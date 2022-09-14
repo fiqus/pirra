7a6df960bad0361afb27aa060960bb3e8f80d037
mjoaco <joaquinmansilla@fiqus.com>
2022-09-14 16:31:58 -0300

    redirect to admin when is logged with teacher account

--- main/views.py
+++ main/views.py
@@ -1,23 +1,26 @@
 # Create your views here.
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response, render
-from django.template import RequestContext
-from empresa.models import Empresa, PuntoDeVenta
-from help.models import LatestNews
+from django.shortcuts import render, redirect
+
+from empresa.models import PuntoDeVenta
 from main.dashboard import get_dashboard_data
+from pirra_web.view import LoginView
 from user.models import ProxiUser
 
 
 @login_required
 def index_customer(request):
-    proxi_user = ProxiUser.objects.get(user=request.user)
-    empresa = proxi_user.company
+    try:
+        proxi_user = ProxiUser.objects.get(user=request.user)
+        empresa = proxi_user.company
 
-    ctx = {
-        # 'ptos_vta': PuntoDeVenta.objects.filter(activo=True).count(),
-        'ptos_vta': PuntoDeVenta.objects.filter(empresa=empresa, activo=True).count(),
-        'empresa_tipos_cbte': empresa.tipos_cbte.count(),
-        'empresa_concepto': 1 if empresa.concepto else 0,
-        'dashboard_data': get_dashboard_data(empresa),
-    }
-    return render(request, 'index_customer.html', ctx)
+        ctx = {
+            # 'ptos_vta': PuntoDeVenta.objects.filter(activo=True).count(),
+            'ptos_vta': PuntoDeVenta.objects.filter(empresa=empresa, activo=True).count(),
+            'empresa_tipos_cbte': empresa.tipos_cbte.count(),
+            'empresa_concepto': 1 if empresa.concepto else 0,
+            'dashboard_data': get_dashboard_data(empresa),
+        }
+        return render(request, 'index_customer.html', ctx)
+    except:
+        return redirect("/admin")
